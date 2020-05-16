4936b1fdd7c725a52d7a38fbe31b0299e776c0f8
turulix <tigga01@online.de>
2020-05-16 22:56:42 +0200

    Update

---
+++ src/app/#components/mainpage/mainpage.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-mainpage',
+    templateUrl: './mainpage.component.html',
+    styleUrls: ['./mainpage.component.scss']
+})
+export class MainpageComponent implements OnInit {
+
+    year = (new Date()).getFullYear();
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MainpageComponent} from './#components/mainpage/mainpage.component';
+
+const appRoutes: Routes = [
+    {path: '', component: MainpageComponent},
+    {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainpageComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
